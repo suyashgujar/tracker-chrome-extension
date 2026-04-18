bb6db774fdc7a073e35ef5882a115cb712b0105a
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-18 17:02:19 +0530

    added input.value

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let myLeads = []
+let myLeads = ["abcd" , "efgh", "ijklo"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click" , function(){
-    console.log("Button Clicked!!")
+    myLeads.push(inputEl.value)
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
