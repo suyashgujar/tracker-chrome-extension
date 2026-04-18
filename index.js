664a5364e449295b22e6b323bdafe9a38e792aa6
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-18 13:57:36 +0530

    updated all three files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function saveInput(){
-    console.log("hello")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click" , function(){
     console.log("Button Clicked!!")
-}
+})
