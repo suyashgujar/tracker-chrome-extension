128219a91da8b1327709cfff26f0613f5ea8f3b5
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-19 20:41:47 +0530

    added input for notes

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-let myLeads = ["abcd" , "efgh", "ijklo"]
+let myLeads = []
+let myNotes = []
 const inputEl = document.getElementById("input-el")
+const noteEl = document.getElementById("note-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
 inputBtn.addEventListener("click" , function(){
     myLeads.push(inputEl.value)
+    myNotes.push(noteEl.value)
+    noteEl.value = ""
+    inputEl.value = ""
+    renderLeads()
+   
 })
 
-for(let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+
+    listItems += `
+    <li>
+     <a target="_blank" href="${myLeads[i]}">
+     ${myLeads[i]}    
+     </a>
+      ---- ${myNotes[i]}
+    </li>
+    `
+    }
+  ulEl.innerHTML = listItems
+
 }
+
+
+
