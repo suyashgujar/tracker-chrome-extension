835c428d26003d522415f09310cdced45a1771df
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-21 00:09:27 +0530

    added tab button

--- index.js
+++ index.js
@@ -3,16 +3,53 @@ let myNotes = []
 const inputEl = document.getElementById("input-el")
 const noteEl = document.getElementById("note-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 
+const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const noteFromLocalStorage = JSON.parse(localStorage.getItem("myNotes"))
+
+const tabs = [{url:"www.facebook.com"}]
+
+
+if(leadFromLocalStorage && noteFromLocalStorage){
+  myLeads = leadFromLocalStorage
+  myNotes = noteFromLocalStorage
+  renderLeads()
+}
 
 inputBtn.addEventListener("click" , function(){
+ 
     myLeads.push(inputEl.value)
     myNotes.push(noteEl.value)
     noteEl.value = ""
     inputEl.value = ""
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+    localStorage.setItem("myNotes" , JSON.stringify(myNotes))
     renderLeads()
-   
+
+  
+})
+
+deleteBtn.addEventListener("dblclick" , function(){
+
+  localStorage.clear()
+  myLeads = []
+  myNotes = []
+  renderLeads()
+    
+})
+
+tabBtn.addEventListener("click" , function(){
+
+  myLeads.push(tabs[0].url)
+  myNotes.push(noteEl.value)
+  noteEl.value = ""
+  localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+  localStorage.setItem("myNotes" , JSON.stringify(myNotes))
+  renderLeads()
+
 })
 
 
@@ -25,7 +62,7 @@ function renderLeads(){
      <a target="_blank" href="${myLeads[i]}">
      ${myLeads[i]}    
      </a>
-      ---- ${myNotes[i]}
+        ----> ${myNotes[i]}
     </li>
     `
     }
